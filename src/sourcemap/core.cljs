730d6de1078826fb2e8a56dc696dfe9a77fd3c1f
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-12-14 23:06:22 -0800

    Add VLQ base64 parsing.

--- src/sourcemap/core.cljs
+++ src/sourcemap/core.cljs
@@ -6,6 +6,9 @@
 
 (enable-console-print!)
 
+(defn get-file [file done]
+  (.then (xhr/get file) done))
+
 (defn build-text-element [text]
   (letfn [(build-line [line]
             (let [element (d/create-element :p)
@@ -27,9 +30,43 @@
         (apply d/append! element lines-els))
       (d/add-class! element "code"))))
 
-(.then (xhr/get "/source.js")
-       #(d/append! (d/sel1 "#source-code") (build-text-element %)))
+(get-file "/source.js"
+          #(d/append! (d/sel1 "#source-code") (build-text-element %)))
 
-(.then (xhr/get "/source.min.js")
-       #(d/append! (d/sel1 "#minified-code") (build-text-element %)))
+(get-file "/source.min.js"
+          #(d/append! (d/sel1 "#minified-code") (build-text-element %)))
+
+(defn b64-char-to-num [char]
+  (.indexOf "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=" char))
+
+(defn acc-to-num [acc]
+  (let [res (reduce #(+ (* %1 32) %2) (reverse acc))]
+    (if (even? res)
+      (bit-shift-right res 1)
+      (- (bit-shift-right res 1)))))
+
+(defn parse-vlq [str]
+  (loop [acc []
+         results []
+         left (map b64-char-to-num str)]
+    (if (empty? left)
+      results
+      (let [[cur & left] left
+            acc (conj acc (bit-and cur 31))]
+        (if (< cur 32)
+          (recur [] (conj results (acc-to-num acc)) left)
+          (recur acc results left))))))
+
+(println [0,11,2,9,7,6,2,4,-9,11,7,-8,5,9])
+(println (parse-vlq "AWESOMEITWORKS"))
+
+(println [1233232, 10101313, 131223])
+(println (parse-vlq "g1orCikxoTupgI"))
+
+(get-file
+ "/source.min.js.map"
+ (fn [text]
+   (let [sm (js->clj (.parse js/JSON text)
+                     :keywordize-keys true)]
+     (println sm))))
 
