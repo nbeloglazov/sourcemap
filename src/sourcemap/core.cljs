229a1719d1ec055136550f239e450eea769dda19
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-12-15 22:20:12 -0800

    Parse mappings and highlite entries in source code.

--- src/sourcemap/core.cljs
+++ src/sourcemap/core.cljs
@@ -57,16 +57,68 @@
           (recur [] (conj results (acc-to-num acc)) left)
           (recur acc results left))))))
 
-(println [0,11,2,9,7,6,2,4,-9,11,7,-8,5,9])
-(println (parse-vlq "AWESOMEITWORKS"))
+(println (= (parse-vlq "AWESOMEITWORKS")
+            [0,11,2,9,7,6,2,4,-9,11,7,-8,5,9]))
 
-(println [1233232, 10101313, 131223])
-(println (parse-vlq "g1orCikxoTupgI"))
+(println (= (parse-vlq "g1orCikxoTupgI")
+            [1233232, 10101313, 131223]))
+
+(defn to-entry [sourcemap vlq]
+  (let [[col src-file src-row src-col src-name] vlq]
+    {:col col
+     :src-file (if src-file
+                 (nth (:sources sourcemap) src-file)
+                 nil)
+     :src-col src-col
+     :src-row src-row
+     :src-name (if src-name
+                 (nth (:names sourcemap) src-name)
+                 nil)}))
+
+(defn parse-line [sourcemap base line]
+  (loop [entries []
+         base base
+         entry-codes (cstr/split line #",")]
+    (if (empty? entry-codes)
+      [entries base]
+      (let [[head & tail] entry-codes
+            cur (parse-vlq head)
+            entry (to-entry sourcemap (map + base cur))]
+        (recur (conj entries entry)
+               (mapv + base (concat cur (repeat 0)))
+               tail)))))
+
+(defn parse-entries [sourcemap]
+  (loop [entries []
+         ind 0
+         base [0 0 0 0 0]
+         lines (cstr/split (:mappings sourcemap) #";")]
+    (if (empty? lines)
+      entries
+      (let [[head & tail] lines
+            [new-entries new-base] (parse-line sourcemap base head)]
+        (recur (concat entries (map #(assoc % :row ind) new-entries))
+               (inc ind)
+               (assoc new-base 0 0)
+               tail)))))
+
+(defn highlite-entries [entries row-fn col-fn code-el]
+  (doseq [entry entries
+          :let [col (col-fn entry)
+                row (row-fn entry)]
+          :when (and col row)]
+    (println col row (.-id code-el))
+    (-> code-el
+        (d/sel1 (str ".row-" row " .col-" col))
+        (d/add-class! "highlite"))))
 
 (get-file
  "/source.min.js.map"
  (fn [text]
    (let [sm (js->clj (.parse js/JSON text)
-                     :keywordize-keys true)]
-     (println sm))))
+                     :keywordize-keys true)
+         entries (parse-entries sm)]
+     (println entries)
+     (highlite-entries entries :row :col (d/sel1 "#minified-code"))
+     (highlite-entries entries :src-row :src-col (d/sel1 "#source-code")))))
 
